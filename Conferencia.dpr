6f8577ea158a02c1524a866ac629439a9013c26b
rafaellpm <r.lenno.rl@gmail.com>
2021-03-12 17:54:59 -0300

    Update Conferencia.dpr

--- Conferencia.dpr
+++ Conferencia.dpr
@@ -3,7 +3,9 @@ program Conferencia;
 uses
   Vcl.Forms,
   UConfig in 'Upas\UConfig.pas',
-  UConferencia in 'Forms\UConferencia.pas' {Form1};
+  UConferencia in 'Forms\UConferencia.pas' {Form1},
+  UConsultaProduto in 'Forms\UConsultaProduto.pas' {Form2},
+  UConsultaVenda in 'Forms\UConsultaVenda.pas' {Form3};
 
 {$R *.res}
 
@@ -11,5 +13,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm3, Form3);
   Application.Run;
 end.
